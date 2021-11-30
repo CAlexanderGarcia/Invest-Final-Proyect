5a2926431f1a38baa2d2d353b33ae7560801b7a3
CAlexanderGarcia <calexandergarcia@gmail.com>
2021-11-30 18:04:07 +0000

    Fondo Foto

---
+++ src/front/js/component/button.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+const Button = e => {
+	return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Button;
---
+++ src/front/js/component/jumbotron.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Jumbotron = () => {
+	return (
+		<div className="container-fluid first-Jumbotron">
+			<h1 className="text-white title text-center">JAF</h1>
+
+			<button className="" />
+		</div>
+	);
+};
+
+export default Jumbotron;
+
+/*<p> La mejor herramienta para controlar los movimientos de tu empresa </p>*/
