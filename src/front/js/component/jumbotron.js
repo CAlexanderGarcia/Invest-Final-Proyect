e58cc41ff021918a438081d769b53332e45f2fae
CAlexanderGarcia <calexandergarcia@gmail.com>
2021-12-07 18:46:45 +0000

    front_2

--- src/front/js/component/jumbotron.js
+++ src/front/js/component/jumbotron.js
@@ -1,13 +1,17 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-const Jumbotron = () => {
+const Jumbotron = props => {
 	return (
 		<div className="container-fluid first-Jumbotron">
-			<h1 className="text-white title text-center">JAF</h1>
+			<h1 className="text-white title text-center">{props.title}</h1>
 		</div>
 	);
 };
 
+Jumbotron.propTypes = {
+	title: PropTypes.string
+};
 export default Jumbotron;
 
 /*<p> La mejor herramienta para controlar los movimientos de tu empresa </p>*/
--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -2,13 +2,15 @@ import React, { useContext } from "react";
 import { Context } from "../store/appContext";
 import "../../styles/home.scss";
 import Jumbotron from "../component/jumbotron.js";
+import Jumbotron_2 from "../component/jumbotron-2.js";
 
 export const Home = () => {
 	const { store, actions } = useContext(Context);
 
 	return (
 		<div>
-			<Jumbotron />
+			<Jumbotron title="JAF" />
+			<Jumbotron_2 />
 		</div>
 	);
 };
