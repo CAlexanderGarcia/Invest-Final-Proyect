e56b55dc0749d1e5458f413951cf337e27dac390
jddelvillar <delvillar2017.j@gmail.com>
2021-12-07 18:40:13 +0000

    third commit

--- src/api/models.py
+++ src/api/models.py
@@ -7,6 +7,39 @@ class User(db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    name = db.Column(db.String, nullable=False)
+    lastname = db.Column(db.String, nullable=False)
+    position_id = db.Column(db.Integer, db.ForeignKey('position.id'), nullable=False)
+
+    position = db.relationship('Position', backref='user', lazy=True)
+
+    def __repr__(self):
+        return '<User %r>' % self.name
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "email": self.email,
+            "name": self.name,
+            # do not serialize the password, its a security breach
+        }
+
+class Position(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=True, nullable=False)
+
+    def __repr__(self):
+        return '<Position %r>' % self.name
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,           
+        }
+
+        #Mario, mario@email.com, 12312312, Administrador
+        #Elena, elena@email.com, 12312334, gerente
+
 
     def __repr__(self):
         return '<User %r>' % self.username
--- src/front/js/component/form.js
+++ src/front/js/component/form.js
@@ -1,46 +1,112 @@
 import React from "react";
+import React, { useContext, useEffect, useState } from "react";
 
 const Form = () => {
+	const { store, actions } = useContext(Context);
+	const [data, setData] = useState({
+		email: "",
+		name: "",
+		password: ""
+	});
+
+	const handleInputChange = event => {
+		let value = event.target.value;
+		if (event.target.name == "usuario") {
+			value = parseInt(value);
+		}
+		setData({
+			...data,
+			[event.target.name]: value
+		});
+	};
+
 	return (
-		<form className="row g-3 ms-4 me-4">
-			<div className="row g-3">
-				<div className="col">
-					<label htmlFor="name" className="form-label">
-						Nombre
-					</label>
-					<input type="text" className="form-control" placeholder="Nombre" aria-label="First name" />
-				</div>
-				<div className="col">
-					<label htmlFor="lastName" className="form-label">
-						{" "}
-						Apellidos{" "}
-					</label>
-					<input type="text" className="form-control" placeholder="Apellidos" aria-label="Last name" />
-				</div>
-			</div>
-			<div className="col-md-6">
-				<label htmlFor="inputEmail4" className="form-label">
-					Email
+		<form action="" className="formulario row g-3 ms-4 me-4" id="formulario">
+			<div className="formulario__grupo row g-3" id="grupo__usuario">
+				<label htmlFor="usuario" className="form-label">
+					Usuario
 				</label>
-				<input type="email" className="form-control" id="inputEmail4" />
+				<div className="formulario__grupo-input">
+					<input
+						type="text"
+						className="form-input"
+						id="usuario"
+						placeholder="Tu usuario"
+						name="usuario"
+						onChange={handleInputChange}
+					/>
+					<i className="formulario__validacion-estado far fa-times-circle" />
+				</div>
+				<p className="formulario__input-error">
+					El usuario tiene que ser de 4 a 21 dígitos y solo puede contener letras y guión
+				</p>
 			</div>
-			<div className="col-6">
-				<label htmlFor="inputAddress" className="form-label">
+
+			<div className="formulario__grupo row g-3" id="grupo__nombre">
+				<label htmlFor="nombre" className="form-label">
+					Nombre
+				</label>
+				<div className="formulario__grupo-input">
+					<input type="text" className="form-input" id="nombre" placeholder="John" name="First name" />
+					<i className="formulario__validacion-estado far fa-times-circle" />
+				</div>
+				<p className="formulario__input-error">
+					El nombre tiene que ser de 4 a 21 dígitos y solo puede contener letras y guión
+				</p>
+			</div>
+
+			<div className="formulario__grupo" id="grupo__apellidos">
+				<label htmlFor="apellidos" className="form-label">
+					Apellidos
+				</label>
+				<div className="formulario__grupo-input">
+					<input type="text" className="form-input" id="apellidos" placeholder="Doe" name="Last name" />
+					<i className="formulario__validacion-estado far fa-times-circle" />
+				</div>
+				<p className="formulario__input-error">
+					El apellidos tiene que ser de 4 a 21 dígitos y solo puede contener letras y guión
+				</p>
+			</div>
+
+			<div className="formulario__grupo" id="grupo__correo">
+				<label htmlFor="e-mail" className="form-label">
+					Correo electrónico
+				</label>
+				<div className="formulario__grupo-input">
+					<input
+						type="email"
+						className="form-input"
+						id="e-mail"
+						placeholder="correo@correo.com"
+						name="e-mail"
+					/>
+					<i className="formulario__validacion-estado far fa-times-circle" />
+				</div>
+				<p className="formulario__input-error">El e-mail tiene que tener puntos, guiones, letras y @</p>
+			</div>
+
+			<div className="formulario__grupo" id="grupo__apellidos">
+				<label htmlFor="adress" className="form-label">
 					Dirección de la empresa
 				</label>
-				<input type="text" className="form-control" id="inputAddress" placeholder="Calle Pepito Perez, 12" />
+				<div className="formulario__grupo-input">
+					<input type="text" className="form-input" id="adress" placeholder="Doe" aria-label="adress" />
+					<i className="formulario__validacion-estado far fa-times-circle" />
+				</div>
+				<p className="formulario__input-error">Dirección de la empresa debe contener letras y numeros</p>
 			</div>
+
 			<div className="col-md-6">
 				<label htmlFor="inputCompany" className="form-label">
 					Razón Social
 				</label>
-				<input type="text" className="form-control" id="inputCompany" />
+				<input type="text" className="form-input" id="inputCompany" />
 			</div>
 			<div className="col-md-6">
 				<label htmlFor="specificSizeInputDocNumber" className="form-label">
 					Numero de identificación
 				</label>
-				<input type="text" className="form-control" id="specificSizeInputDocNumber" />
+				<input type="text" className="form-input" id="specificSizeInputDocNumber" />
 			</div>
 			<div className="col-md-4">
 				<label htmlFor="inputState" className="form-label">
@@ -53,28 +119,52 @@ const Form = () => {
 					<option>NIF</option>
 				</select>
 			</div>
-			<div className="col-md-2">
-				<label htmlFor="inputZip" className="form-label">
-					Código Postal
+
+			<div className="formulario__grupo" id="grupo__password">
+				<label htmlFor="password" className="form-label">
+					Contraseña
 				</label>
-				<input type="text" className="form-control" id="inputZip" />
+				<div className="formulario__grupo-input">
+					<input type="password" className="form-input" id="password" name="password" />
+					<i className="formulario__validacion-estado far fa-times-circle" />
+				</div>
+				<p className="formulario__input-error">
+					la contraseña debe tener de 4 a 12 dígitos, debe contener letras y números
+				</p>
 			</div>
-			<div className="col-md-6">
-				<label htmlFor="inputPassword4" className="form-label">
-					Password
+
+			<div className="formulario__grupo" id="grupo__password2">
+				<label htmlFor="password2" className="form-label">
+					Repetir Contraseña
 				</label>
-				<input type="password" className="form-control" id="inputPassword4" />
+				<div className="formulario__grupo-input">
+					<input type="password" className="form-input" id="password2" name="password2" />
+					<i className="formulario__validacion-estado far fa-times-circle" />
+				</div>
+				<p className="formulario__input-error">ambas contraseñas deben coincidir</p>
 			</div>
-			<div className="col-md-6">
-				<label htmlFor="inputPassword4" className="form-label">
-					Confirm Password
+
+			<div className="formulario__grupo-terminos" id="grupo__terminos">
+				<label className="form-label">
+					<input className="formulario__checkbox" type="checkbox" name="terminos" id="terminos" />
+					Acepto terminos y condiciones
 				</label>
-				<input type="password" className="form-control" id="inputPassword4" />
 			</div>
-			<div className="col-12">
+
+			<div className="formulario__mensaje" id="formulario__mensaje">
+				<p>
+					<i className="fas fa-exclamation-triangle" /> <b>Error:</b> Por favor rellena el formulario de forma
+					correcta
+				</p>
+			</div>
+
+			<div className="formulario__grupo formulario__grupo-btn-enviar">
 				<button type="submit" className="btn btn-primary">
 					Registrarse
 				</button>
+				<p className="formulario__mensaje-exito" id="formulario__mensaje-exito">
+					Registro exitoso
+				</p>
 			</div>
 		</form>
 	);
