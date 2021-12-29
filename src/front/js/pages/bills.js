ea6fa338d545c7284327f26b8d3900e3b8b2f358
Fredy Moreno <info@fredymoreno.es>
2021-12-29 17:42:36 +0000

    frontend form finish

---
+++ src/front/js/pages/bills.js
@@ -0,0 +1,10 @@
+import React from "react";
+import BillsForm from "../component/billsForm";
+
+export const Bills = () => {
+	return (
+		<div className="container">
+			<BillsForm />
+		</div>
+	);
+};
